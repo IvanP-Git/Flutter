f4db702cba0a95bb0122d795fe982c154dddc7c7
IvanDev2025 <206088520+IvanP-Git@users.noreply.github.com>
2026-01-12 16:05:41 +0100

    Remove Lista_Actores

--- movies2526-main/movies2526-main/lib/api/api.dart
+++
@@ -1,5 +0,0 @@
-class Api {
-  static const baseUrl = "https://api.themoviedb.org/3/"; // URL Base de la Api
-  static const imageBaseUrl = "https://image.tmdb.org/t/p/original/"; // URL Base de las imágenes
-  static const apiKey = "4317234f24ac359e22bb7192a512124b"; // Clave de la Api
-}
--- movies2526-main/movies2526-main/lib/api/api_end_points.dart
+++
@@ -1,7 +0,0 @@
-class ApiEndPoint {
-  static const products = "products"; // Endpoint para obtener productos
-  static const popularMovies = "movie/popular"; // Endpoint para obtener películas populares
-  static const upcomingMovies = "movie/upcoming"; // Endpoint para obtener películas próximas
-  static const getGenreList = "genre/movie/list"; // Endpoint para obtener la lista de géneros
-  static const popularActors = "person/popular"; // Endpoint para obtener actores populares
-}
